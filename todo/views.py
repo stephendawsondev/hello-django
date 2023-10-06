8e5aa844f0406bc4394c8208b6437dcbd401f996
Stephen Dawson <stephendawson@protonmail.com>
2023-10-06 20:47:31 +0100

    add todos to template on frontend

--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
-
+from .models import Item
 # Create your views here.
+
+
 def get_todo_list(request):
-    return render(request, 'todo/todo_list.html')
+    context = {
+        'items': Item.objects.all()
+    }
+    return render(request, 'todo/todo_list.html', context)
